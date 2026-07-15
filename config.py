612fb688cae12f3b53ee58ababb08d26e3d8e373
Harkiran Sahota <harkiran.sahota@gmx.de>
2026-07-15 13:02:24 +0000

    Added Application Memory

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv()
 API_KEY = os.getenv("NEBIUS_API_KEY")
 MODEL_NAME = os.getenv("MODEL_NAME")
 BASE_URL = os.getenv("BASE_URL")
+
+MAX_HISTORY=os.getenv("MAX_HISTORY")
--- llm.py
+++ llm.py
@@ -6,7 +6,7 @@ from prompts import SYSTEM_PROMPT
 from models import AssemblyInstructions
 
 class ManufacturingCopilot:
-
+   
     def __init__(self):
 
         self.client = OpenAI(
@@ -14,25 +14,49 @@ class ManufacturingCopilot:
             base_url=BASE_URL
         )
 
+        self.messages = [
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            }
+        ]
+
     def ask(self, question):
         print("ask question")
+
+        #add new question to message history
+        self.messages.append(
+            {
+                "role": "user",
+                "content": question
+            }
+        )
+
         completion = self.client.chat.completions.parse(
 
             model=MODEL_NAME,
             
-            messages=[
-                {
-                    "role": "system",
-                    "content": SYSTEM_PROMPT
-                },
-                {
-                    "role": "user",
-                    "content": question
-                }
-            ],
+            #send message history
+            messages=self.messages
             
             response_format=AssemblyInstructions
         )
         print("return result")
 
-        return completion.choices[0].message.parsed
+        instructions = completion.choices[0].message.parsed
+        #add response to message history
+        self.messages.append(
+            {
+                "role": "assistant",
+                "content": instructions.model_dump_json()
+            }
+        )
+
+        #check for correct history length
+        if len(self.messages) > MAX_HISTORY + 1:
+            self.messages = (
+                [self.messages[0]]
+                + self.messages[-MAX_HISTORY:]
+            )
+
+        return instructions
